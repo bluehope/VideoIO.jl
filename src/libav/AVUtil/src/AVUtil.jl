c3e99d684bfd3adbcc33ad1d211493cc8be040bb
Kevin Squire <kevin.squire@gmail.com>
2015-06-29 02:42:00 -0700

    Actually export new AVUtil constant definitions

--- src/libav/AVUtil/src/AVUtil.jl
+++ src/libav/AVUtil/src/AVUtil.jl
@@ -6,7 +6,10 @@ module AVUtil
 
   Base.zero(::Type{AVRational}) = AVRational(0, 1)
 
-  # for compatibility with ffmpeg
+# for compatibility with ffmpeg
+# Note that these constants aren't actually defined by libav,
+# but they will allow us to use them without error
+
   const AV_OPT_TYPE_IMAGE_SIZE = @compat UInt32(1397316165)
   const AV_OPT_TYPE_PIXEL_FMT = @compat UInt32(1346784596)
   const AV_OPT_TYPE_SAMPLE_FMT = @compat UInt32(1397116244)
@@ -14,6 +17,13 @@ module AVUtil
   const AV_OPT_TYPE_DURATION = @compat UInt32(1146442272)
   const AV_OPT_TYPE_COLOR = @compat UInt32(1129270354)
   const AV_OPT_TYPE_CHANNEL_LAYOUT = @compat UInt32(1128811585)
+
+export
+  AV_PIX_FMT_0RGB,
+  AV_PIX_FMT_RGB0,
+  AV_PIX_FMT_0BGR,
+  AV_PIX_FMT_BGR0
+
 
   const AV_PIX_FMT_0RGB = @compat Int32(295)
   const AV_PIX_FMT_RGB0 = @compat Int32(296)
